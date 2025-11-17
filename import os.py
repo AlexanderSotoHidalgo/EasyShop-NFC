0bc7cbd033e27251b4f38cd8463f141139c27c66
AlexanderSotoHidalgo <alexander.sotoh@unmsm.edu.pe>
2025-11-16 22:23:26 -0500

    Delete import os.py

--- import os.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-def contar_archivos_pathlib(directorio):
-    return sum(1 for archivo in Path(directorio).rglob('*') if archivo.is_file())
-
-# Ejemplo de uso:
-directorio = r'C:\ruta\de\tu\directorio'
-print(f"Número total de archivos: {contar_archivos_pathlib(directorio)}")
